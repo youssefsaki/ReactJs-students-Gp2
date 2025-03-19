7d3619c0d05791b2c07b53eede4a69b2a6d997f2
Oussama <saidi.oussama2004@gmail.com>
2025-03-19 16:07:37 +0100

    updates

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,9 @@
-import ChangeName from "./components/ChangeName"
-import Counter from "./components/Counter"
-import Form from "./components/Form"
 
 
 const App = () => {
   return (
     <div>
-      {/* <Form /> */}
-      {/* <ChangeName /> */}
-      <Counter />
+      Hello World
     </div>
   )
 }
